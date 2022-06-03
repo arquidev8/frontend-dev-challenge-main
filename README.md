6387cb9309f5f4b25893c942c2958fe3ca036443
Hector Hernandez <98498528+arquidev8@users.noreply.github.com>
2022-06-03 17:40:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<p align="center">
+  <a href='https://weremote.net'>
+    <img src="https://weremote.net/wp-content/uploads/2021/04/Logo-WR.svg" width="300" />
+  </a>
+</p>
+<br />
+
+## Descripción
+**Frontend DEV Challenge** prueba de admisión para desarrolladores en HTML, CSS, JavaScript.
+Participante Hector Hernandez
+
---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+/* example function name */
+// const openAndCloseAccordion = () => {
+//   alert ('do something')
+// }
+
+const menu = document.querySelector('.hamburger');
+
+function toggleMenu (event) {
+  this.classList.toggle('is-active');
+  document.querySelector( ".menuppal" ).classList.toggle("is_active");
+  event.preventDefault();
+}
+
+// event
+menu.addEventListener('click', toggleMenu, false);
+
+
+
+let answers = document.querySelectorAll(".accordion");
+answers.forEach((event) => {
+  event.addEventListener("click", () => {
+    if (event.classList.contains("active")) {
+      event.classList.remove("active");
+    } else {
+      event.classList.add("active");
+    }
+  });
+});
